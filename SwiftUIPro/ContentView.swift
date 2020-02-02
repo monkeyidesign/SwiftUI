d738a6b99662c0b485b66b055dac4d64a1465c9b
monkeyidesign <sam@monkeyidesign.com>
2020-02-02 14:46:08 +0700

    Initial Commit

---
+++ SwiftUIPro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPro
+//
+//  Created by Apple on 2/2/20.
+//  Copyright © 2020 Monkey iDesign. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
